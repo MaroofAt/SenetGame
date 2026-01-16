b6d7afc7c2d994cd33b4cca0deb2a471451a1eec
MaroofAt <maroof95at@gmail.com>
2026-01-16 14:14:42 +0300

    Edited State added apply_action and is_terminal , Edited Board , Edited Main for testing

--- src/exec/Main.java
+++ src/exec/Main.java
@@ -1,6 +1,9 @@
 package exec;
 
 import structure.Board;
+import logic.State;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,8 +11,25 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Initializing Senet board...\n");
 
-        Board board = new Board();
-        System.out.println(board);
+//        Board board = new Board();
+//        System.out.println(board);
+        State state = new State();
+        System.out.println(state);
+        state = state.move_piece(13, 12);
+        System.out.println(state);
+        state = state.move_piece(25, 2);
+        System.out.println(state);
+        state = state.move_piece(11, 3);
+        System.out.println(state);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press Enter");
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.isEmpty()) {
+                break;
+            }
+        }
+        System.out.println(state);
 
 //        System.out.println("structure.Board initialized successfully!");
     }
